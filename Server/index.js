12d87cbead2de4975e056ff7fb9fa7d6512f508a
Ayushee Roy <ayusheeroy05@gmail.com>
2024-05-02 04:34:21 +0530

    init

---
+++ Server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
+const TodoModel = require('./Models/Todo')
+const app = express()
+app.use(cors())
+app.use(express.json()) 
+
+mongoose.connect("mongodb+srv://AyusheeRoy:Vincent%4005@cluster0.4v3xuk2.mongodb.net/test")
+app.get('/get', (req, res)=> {
+    TodoModel.find().then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+  
+app.post('/add', (req, res) => {
+    const task = req.body.task;
+    TodoModel.create({
+        task: task
+    }).then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.listen(3001);
---
+++ todolist/src/Create.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+export default Create 
+function Create() {
+    const [task, setTask] = useState()
+    const handleAdd = () => {
+        axios.post("http://localhost:3001/add", {
+            task: task
+        }).then(() => {
+            console.log("success")
+        })}
+  return (
+    <div className='create_form'>
+        <input type="text" placeholder="Enter Task" onChange={(e) => setTask(e.target.value)}/>
+        <button type="button" onClick= {handleAdd}>Add</button>
+    </div>
+    )
+}
